f0dab9a3f878c1d23600b41f1d53eb542157a4f4
Jatin Patel <jatinpatel136@gmail.com>
2020-12-14 15:46:58 +0530

    Populate bootcamps for courses, reverse populate courses for bootcamps and cascade delete courses on deletion of bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -25,7 +25,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
     // finding resource
-    query = Bootcamp.find(JSON.parse(queryString));
+    query = Bootcamp.find(JSON.parse(queryString)).populate('courses');
 
     // Select fields
     if (req.query.select) {
@@ -118,11 +118,13 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp with ${req.params.id} not found`, 404));
     }
+
+    bootcamp.remove();
 
     res.status(200).json({ success: true, data: {} });
 });
