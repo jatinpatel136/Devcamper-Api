4d3d354b42e1e0d0e2a473a5679f4d0f17991a4f
Jatin Patel <jatinpatel136@gmail.com>
2020-12-16 14:08:23 +0530

    sign and get json web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,9 +8,13 @@ const User = require('../models/Users');
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body;
 
+    // Create User
     const user = await User.create({
         name, email, password, role
     });
 
-    res.status(200).json({ success: true });
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token });
 })
