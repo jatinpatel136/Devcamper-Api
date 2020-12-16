2890e20ae542ca8f299a12d349c8966a4f095017
Jatin Patel <jatinpatel136@gmail.com>
2020-12-16 14:38:50 +0530

    code for user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/Users');
 
 // @desc    Register user
-// @route   GET /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body;
@@ -12,6 +12,38 @@ exports.register = asyncHandler(async (req, res, next) => {
     const user = await User.create({
         name, email, password, role
     });
+
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token });
+})
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // validate email and password
+    if (!email || !password) {
+        return next(new ErrorResponse(`Please provide and email and passwors`, 400));
+    }
+
+    // Check for user
+    // NOTE: added .select because we have set select password to false in database model by default so we need to explicitly select it here
+    const user = await User.findOne({ email: email }).select('+password');
+
+    if (!user) {
+        return next(new ErrorResponse(`Invalid credentials`, 401));
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
 
     // Create token
     const token = user.getSignedJwtToken();
