560720e133d68e409091b20dee61322934da9f68
Jatin Patel <jatinpatel136@gmail.com>
2020-12-16 13:02:41 +0530

    added user model, user register and password encryption

---
+++ controllers/auth.js
@@ -0,0 +1,16 @@
+const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/Users');
+
+// @desc    Register user
+// @route   GET /api/v1/auth/register
+// @access  Public
+exports.register = asyncHandler(async (req, res, next) => {
+    const { name, email, password, role } = req.body;
+
+    const user = await User.create({
+        name, email, password, role
+    });
+
+    res.status(200).json({ success: true });
+})
