c0306ad5b5dfd7e1054be1f7b21443808b6b8e4c
Jatin Patel <jatinpatel136@gmail.com>
2020-12-17 20:08:13 +0530

    update user details and password functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { register, login, getLoggedInUser, forgotPassword, resetPassword } = require('../controllers/auth');
+const { register, login, getLoggedInUser, forgotPassword, resetPassword, updateDetails, updatePassword } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/loggedInUser', protect, getLoggedInUser)
+router.get('/loggedInUser', protect, getLoggedInUser);
+router.put('/updateDetails', protect, updateDetails);
+router.put('/updatePassword', protect, updatePassword)
 router.post('/forgotPassword', forgotPassword);
 router.put('/resetPassword/:resetToken', resetPassword);
 
